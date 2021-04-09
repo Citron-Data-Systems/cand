cd23f91da00e53236a78c644b410d4680dcd4b73
alde103 <aldebaran_103@hotmail.com>
2021-04-09 15:19:12 -0500

    Refactor started

--- mix.exs
+++ mix.exs
@@ -1,28 +1,51 @@
 defmodule Cand.Mixfile do
   use Mix.Project
 
+  @version "0.1.1"
+  @source_url "https://github.com/valiot/cand"
+
+
   def project do
     [
       app: :cand,
-      version: "0.1.0",
-      build_path: "_build",
-      deps_path: "deps",
-      lockfile: "mix.lock",
-      elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
+      version: @version,
+      elixir: "~> 1.9",
+      name: "Cand",
+      description: description(),
+      package: package(),
+      source_url: @source_url,
+      start_permanent: Mix.env() == :prod,
+      build_embedded: true,
       deps: deps()
+    ]
+  end
+
+  defp description() do
+    "Socketcand (SocketCAN Daemon) elixir wrapper."
+  end
+
+  defp package() do
+    [
+      files: [
+        "lib",
+        "test",
+        "mix.exs",
+        "README.md",
+      ],
+      maintainers: ["valiot"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 
   def application do
     [
       extra_applications: [:logger],
-      mod: {Cand.Application, []}
     ]
   end
 
   defp deps do
     [
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
     ]
   end
 end
